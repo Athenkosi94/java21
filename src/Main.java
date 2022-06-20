f953be69c991da6980669c248533bc9f8866d14e
user <athimbityana@gmail com>
2022-06-20 13:33:08 +0100

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int i;
+
+        for(i=19;i>10;--i){
+
+            {
+                System.out.println(i);
+
+            }
+        }
+    }
+}
